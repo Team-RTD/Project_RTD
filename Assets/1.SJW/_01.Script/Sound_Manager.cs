14bad941e3e3e1769b2a5336698901b9c795529c
ARENA\arena5045 <dnwjdtj10@naver.com>
2023-09-05 01:16:03 +0900

    사운드매니저 추가

---
+++ Assets/1.SJW/_01.Script/Sound_Manager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+    public static Sound_Manager instance { get; private set; }
+
+
+    AudioClip[] bgms;
+
+    public AudioSource bgmPlayer;
+    public AudioSource effectSoundPlayer;
+    public AudioSource narSoundPlayer;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BgmPlay(AudioClip bgm)
+    {
+        bgmPlayer.Stop();
+        bgmPlayer.clip = bgm;
+        bgmPlayer.Play();
+    }
+
+    public void EffectPlay(AudioClip esm)
+    {
+        effectSoundPlayer.PlayOneShot(esm);
+
+    }
+
+    public void NarPlay(AudioClip nar)
+    {
+        narSoundPlayer.clip = nar;
+        narSoundPlayer.Play();
+    }
+
+    
+
+}
