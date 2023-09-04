2492a0dcd1b47359ad12554c49a4a44e49113016
arena5045 <dnwjdtj10@naver.com>
2023-09-04 17:39:09 +0900

    소환진,카메라 완료, 태그 수정

--- Assets/1.SJW/_01.Script/TowerZone.cs
+++ Assets/1.SJW/_01.Script/TowerZone.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TowerZone : MonoBehaviour
 {
+
+    public bool towerOn;
+    public Color[] summonZoneColor = new Color[8];
+
     // Start is called before the first frame update
     void Start()
     {
