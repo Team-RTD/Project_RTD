b3d9c7a24df43f565cddb2ed36b7e93c85641549
MyeunggiKim <139723168+MyeunggiKim@users.noreply.github.com>
2023-09-06 16:41:18 +0900

    Combine 수정중

--- Assets/3.KMK/Scripts/TowerCombine.cs
+++ Assets/3.KMK/Scripts/TowerCombine.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //필드에 미리 생성할 오브젝트가 존재해야만 새로운 타워를 합성할 수 있다.
@@ -7,6 +8,7 @@ using UnityEngine;
 //씬 어딘가 안보이는곳에 타워를 넣어 두기만 하면 잘 작동할 것이다.
 public class TowerCombine : MonoBehaviour
 {
+    //컴포넌트를 무조건 카메라에 넣어둘 것.
     public Camera cam;
     public RaycastHit hit;
 
@@ -19,6 +21,8 @@ public class TowerCombine : MonoBehaviour
 
     public string towerTag3 = "Tower_3";
 
+    public bool combineProsses = false;
+
     public void Start()
     {
         //저장할 빈 배열 선언
@@ -27,9 +31,22 @@ public class TowerCombine : MonoBehaviour
 
     private void Update()
     {
+        if (combineProsses == false)
+        {
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                Combine();
+            }
+        }
+    }
+
+    private void Combine()
+    {        
+        print("확인");
         // 마우스 클릭을 감지
         if (Input.GetMouseButtonDown(0))
         {
+            combineProsses = true;
             // 마우스 클릭한 위치에서 레이를 쏘아 오브젝트 검출
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -65,27 +82,30 @@ public class TowerCombine : MonoBehaviour
 
                                 clickedObjects[1] = null;
                                 clickedObjects[0] = null;
-
-
+                                combineProsses = false;
                             }
                             else
                             {
+                                //타워 합성에 실패 했을 경우 출력.
                                 print("합성 할 수 없는 대상 입니다.");
 
                                 clickedObjects[1] = null;
                                 clickedObjects[0] = null;
+                                combineProsses = false;
                             }
                         }
                     }
                     else
                     {
+                        //동일 타워 2번 선택시 출력.
                         print("다른 타워를 선택해 주십시오.");
                         clickedObjects[1] = null;
+                        combineProsses = false;
                     }
                 }
 
                 //랭크3 타워 합성 시퀸스
-                if (clickedObject.gameObject.tag == "Tower_2")
+                else if (clickedObject.gameObject.tag == "Tower_2")
                 {
                     // 배열에 오브젝트 저장
                     clickedObjects[currentIndex] = clickedObject;
@@ -114,33 +134,52 @@ public class TowerCombine : MonoBehaviour
                                 clickedObjects[1] = null;
                                 clickedObjects[0] = null;
 
-
+                                combineProsses = false;
                             }
                             else
                             {
+                                //타워 합성에 실패 했을 경우 출력.
                                 print("합성 할 수 없는 대상 입니다.");
 
                                 clickedObjects[1] = null;
                                 clickedObjects[0] = null;
+
+                                combineProsses = false;
                             }
                         }
                         else
                         {
+                            //동일 타워 2번 선택시 출력.
                             print("다른 타워를 선택해 주십시오.");
                             clickedObjects[1] = null;
                         }
                     }
                 }
-                if (clickedObject.gameObject.tag == "Tower_3")
+                else if (clickedObject.gameObject.tag == "Tower_3")
                 {
+                    //3티어 타워 선택시 출력.
                     print("해당 타워는 더 이상 합성 할 수 없습니다.");
                     clickedObjects[1] = null;
                     clickedObjects[0] = null;
                 }
+                else
+                {
+                    print("타워를 선택해 주십시요");
+                }
+            }
+            if (Input.GetKey(KeyCode.Alpha2))
+            {
+                combineProsses = false;
+                print("반환합니다.");
+                return;
             }
         }
+
+        print("밖입니다");
+        combineProsses = false;
     }
 
+    //1티어 타워로 2티어 타워를 합성할 경우 실행.
     public void SpawnNewTowerRank2()
     {
         GameObject newTowerObject = FindObjectWithTag("Tower_2");
@@ -152,6 +191,7 @@ public class TowerCombine : MonoBehaviour
             Instantiate(newTowerObject, spawnPosition, spawnRotation);
         }
     }
+    //2티어 타워로 3티어 타워를 합성할 경우 실행.
     public void SpawnNewTowerRank3()
     {
         GameObject newTowerObject = FindObjectWithTag("Tower_3");
@@ -163,7 +203,11 @@ public class TowerCombine : MonoBehaviour
             Instantiate(newTowerObject, spawnPosition, spawnRotation);
         }
     }
+    //추가로 티어를 추가해 더 높은 티어의 타워의 합성이 필요할 경우 복사해서 함수 이름만 바꿔서 작성.
 
+
+
+    //Do not change--------------
     // 특정 태그를 가지는 오브젝트 찾기
     GameObject FindObjectWithTag(string tag)
     {
@@ -175,4 +219,5 @@ public class TowerCombine : MonoBehaviour
         }
         return null;
     }
+    //--------------------------
 }
