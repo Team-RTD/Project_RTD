960c6af20a8fb6de0ddc49a1ba9221da1de982a8
qldtn777 <139726027+qldtn777@users.noreply.github.com>
2023-09-06 15:56:54 +0900

    feat: boss구현,MissionMove 구현
    보스 구현(크기는 안함)
    개인미션
    {
    절반정도만 했음 아직 다 안함
    }

--- Assets/4.OCM/Scripts/MonsterSpawn.cs
+++ Assets/4.OCM/Scripts/MonsterSpawn.cs
@@ -1,6 +1,9 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 //목표1: 적을 생성하는 기능 구현
@@ -13,6 +16,10 @@ using UnityEngine;
 
 //목표3: 몬스터 소환 시 이펙트 생성
 //필요속성3: 이펙트
+
+
+//목표4: z,x,c를 누르면 개인미션1,2,3 을 시작한다.
+//필요속성4: 몬스터1,2,3   미션시간1,2,3
 public class MonsterSpawn : MonoBehaviour
 {
     //필요속성1: 적게임오브젝트,생성시간,총생성시간
@@ -31,6 +38,15 @@ public class MonsterSpawn : MonoBehaviour
     public GameObject spawnEffect0;
 
 
+    //필요속성4: 몬스터1,2,3   미션시간1,2,3
+    public GameObject missionMonster1;
+    public GameObject missionMonster2;
+    public GameObject missionMonster3;
+
+    public int mission1CoolTime;
+    public int mission2CoolTime;
+    public int mission3CoolTime;
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +60,7 @@ public class MonsterSpawn : MonoBehaviour
     // Update is called once per frame
      void Update()
     {
+        
 
         //목표2: 특정시간이 지나고, 몬스터가 다 생성된 다음이고, 몬스터가 다 죽은다음에 다음스테이지로 넘어간다.
         currentTime += Time.deltaTime;
@@ -52,6 +69,21 @@ public class MonsterSpawn : MonoBehaviour
             nextStage = false;
             StartCoroutine(SpawnEnemy(StageManager.instance.stageNum));
             currentTime = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(PersonalMission1());
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(PersonalMission2());
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(PersonalMission3());
         }
 
     }
@@ -64,22 +96,78 @@ public class MonsterSpawn : MonoBehaviour
         Data_Manager.instance.curRound = StageManager.instance.stageNum;
         Ui_Manager.instance.UiRefresh();
         StageManager.instance.monsterCount=20;
-        for (int i = 0; i < 20; i++)
+
+        if (StageManager.instance.stageNum % 10 == 0 && StageManager.instance.stageNum>1)
         {
-            GameObject enemyGO = Instantiate(monsterList[stage-1]);
+            StageManager.instance.monsterCount = 1;
+            GameObject enemyGO = Instantiate(monsterList[stage - 1]);
             enemyGO.transform.position = transform.position;
             enemyGO.transform.rotation = Quaternion.Euler(0, 180, 0);
 
             //목표3: 몬스터 소환 시 이펙트 생성
-            GameObject spawnEffect = Instantiate(spawnEffect0,transform.position, Quaternion.identity);
-            Destroy(spawnEffect, 3f);
+            GameObject spawnEffect = Instantiate(spawnEffect0, transform.position, Quaternion.identity);
+            Destroy(spawnEffect, 0.5f);
             currentTime = 0f;
             yield return new WaitForSeconds(0.3f);
             currentTime = 0f;
-            
         }
+        else
+        {
+            for (int i = 0; i < 20; i++)
+            {
+
+                GameObject enemyGO = Instantiate(monsterList[stage - 1]);
+                enemyGO.transform.position = transform.position;
+                enemyGO.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+                //목표3: 몬스터 소환 시 이펙트 생성
+                GameObject spawnEffect = Instantiate(spawnEffect0, transform.position, Quaternion.identity);
+                Destroy(spawnEffect, 0.5f);
+                currentTime = 0f;
+                yield return new WaitForSeconds(0.3f);
+                currentTime = 0f;
+
+            }
+        }
+
+
+        
         nextStage=true;
         StageManager.instance.stageNum++;
+        StageManager.instance.monsterMaxHp = StageManager.instance.SetMonsterHP(StageManager.instance.stageNum);
+    }
+
+
+
+
+    IEnumerator PersonalMission1()
+    {
+        GameObject enemyGO = Instantiate(missionMonster1);
+        enemyGO.transform.position = transform.position;
+        enemyGO.transform.rotation = Quaternion.Euler(0, 180, 0);
+        GameObject spawnEffect = Instantiate(spawnEffect0, transform.position, Quaternion.identity);
+        Destroy(spawnEffect, 0.5f);
+        yield return null;
+    }
+
+    IEnumerator PersonalMission2()
+    {
+        GameObject enemyGO = Instantiate(missionMonster2);
+        enemyGO.transform.position = transform.position;
+        enemyGO.transform.rotation = Quaternion.Euler(0, 180, 0);
+        GameObject spawnEffect = Instantiate(spawnEffect0, transform.position, Quaternion.identity);
+        Destroy(spawnEffect, 0.5f);
+        yield return null;
+    }
+
+    IEnumerator PersonalMission3()
+    {
+        GameObject enemyGO = Instantiate(missionMonster3);
+        enemyGO.transform.position = transform.position;
+        enemyGO.transform.rotation = Quaternion.Euler(0, 180, 0);
+        GameObject spawnEffect = Instantiate(spawnEffect0, transform.position, Quaternion.identity);
+        Destroy(spawnEffect, 0.5f);
+        yield return null;
     }
 }
 
