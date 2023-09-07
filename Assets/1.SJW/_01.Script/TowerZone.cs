a2f559000241cf4eafa6f12aeed7030446a87394
arena5045 <dnwjdtj10@naver.com>
2023-09-07 17:51:36 +0900

    윤곽선 배치 짱짱맨

--- Assets/1.SJW/_01.Script/TowerZone.cs
+++ Assets/1.SJW/_01.Script/TowerZone.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class TowerZone : MonoBehaviour
 
     public bool towerOn;
     public Color[] summonZoneColor = new Color[8];
+    public GameObject Tower;
+
 
     // Start is called before the first frame update
     void Start()
